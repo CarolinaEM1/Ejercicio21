d33daaa12cf6d63215b9b586e1033154d9d48917
Carolina Martinez <carolinamtz091@gmail.com>
2023-06-19 21:01:00 -0600

    primera version

---
+++ Ejercicio 21.py
@@ -0,0 +1,10 @@
+#Cadena terminada en un punto
+
+frase = input("Digite una frase: ")
+
+if frase.endswith('.'): #Si el ultimo caracter de la cadena es un punto
+    print("La frase termina con un punto")
+else:
+    print("La frase no termina con punto")
+
+#Carolina EM
